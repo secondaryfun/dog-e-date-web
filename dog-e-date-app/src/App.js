6bade4836dd32485561a00254a6ff36163bf54fa
secondaryfun <c.birkenhagen@gmail.com>
2020-06-11 13:51:06 -0400

    Added Kennel Route

--- dog-e-date-app/src/App.js
+++ dog-e-date-app/src/App.js
@@ -6,79 +6,92 @@ import Login from "./components/Login.js"
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-        imgs: [],
-        
+      imgs: [],
+      dogList: [],
+
     }
-    
+
   };
 
-// componentDidMount(){
+  componentDidMount() {
 
-//       let url = "";
+    let url = "https://dog-e-date.herokuapp.com/dog";
 
-//       fetch(url, {
-//         headers: {
-//           Accept: "application/json",
-//         }
-//       })
-//       .then(res => res.json())
-//       .then(data => {
-//         console.log(data)
-//         this.setState({imgs: data.data});
-//       })
-//         .catch(err => {
-//           console.log("Error", err);
-//       });
-//       }
-  
-render(){
+    fetch(url, {
+      headers: {
+        Accept: "application/json",
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        this.setState({ dogList: data.data });
+      })
+      .catch(err => {
+        console.log("Error", err);
+      });
+  }
+
+  render() {
 
 
-  return (
-    <div className="App">
-   
-    <header>
-      <span className="logo">
-      <h2>Dog-E-Date</h2>
-      </span>
-    </header>
-    
-    
-      
-      <div className="container">
+    return (
+      <div className="App">
 
-        <h1>WELCOME TO YOUR NEXT DOG-E-DATE</h1>
-     
-     
-      <div className="button-container">
-      <button className="Login">LOG IN</button>
-      <button className="SignUp">SIGN UP</button>
+        <header>
+          <span className="logo">
+            <h2>Dog-E-Date</h2>
+          </span>
+        </header>
+
+
+
+        <div className="container">
+
+          <h1>WELCOME TO YOUR NEXT DOG-E-DATE</h1>
+
+
+          <div className="button-container">
+            <button className="Login">LOG IN</button>
+            <button className="SignUp">SIGN UP</button>
+            <button className="SignUp">That's One Sexy Dog</button>
+
+          </div>
+
+          <Route
+            path="/login/"
+            render={() => (
+              null
+            )
+            }
+          />
+
+          <Route
+            path="/signup/"
+            render={() => (
+              null
+            )
+            }
+          />
+          <Route
+            path="/kennel/"
+            render={() => (
+              null
+              // <Kennel dogList={this.state.dogList} />
+            )
+            }
+          />
+
+
+
+        </div>
+
       </div>
-    
-      <Route 
-        path="/login/"
-        render={ () => ( null
-        )
-        }
-      />
-
-      <Route 
-        path="/signup/"
-        render= { () => ( null
-        ) 
-        }
-      />
-
-     
-
-      </div>
-      
-    </div>
-  );
-}
+    );
+  }
 }
 export default App;
