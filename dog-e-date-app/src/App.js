a010d99a348ad98fe3776bb16438ab98a54ff428
B CASTILLO <bryancastillo44@outlook.com>
2020-06-11 08:57:21 -0400

    Added images and updated buttons

--- dog-e-date-app/src/App.js
+++ dog-e-date-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-// import Dog from './dog.js';
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
 
@@ -8,40 +8,53 @@ class App extends Component {
     super(props);
 
     this.state = {
-        dog: '',
+        imgs: [],
+        
     }
     
   };
 
-componentDidMount(){
+// componentDidMount(){
 
-      let url = "https://dog.ceo/api/breeds/image/random/2ad32a52-5d71-4e75-bbf6-f49b53c77888";
+//       let url = "https://dog.ceo/api/breeds/image/random/2ad32a52-5d71-4e75-bbf6-f49b53c77888";
 
-      fetch(url, {
-        headers: {
-          Accept: "application/json",
-        }
-      })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        this.setState({
-          dogs: data,
-        });
-      });
-    
-      console.log("componentDidMount")
-    }
+//       fetch(url, {
+//         headers: {
+//           Accept: "application/json",
+//         }
+//       })
+//       .then(res => res.json())
+//       .then(data => {
+//         console.log(data)
+//         this.setState({imgs: data.data});
+//       })
+//         .catch(err => {
+//           console.log("Error", err);
+//       });
+//       }
   
 render(){
 
 
   return (
     <div className="App">
+   
+    <header>
+      <span class="logo">
+      <h2>Dog-E-Date</h2>
+      </span>
+    </header>
+    
+    <div className="login-button">
+      <button className="Login">LOG IN</button>
+      </div>
+      
       <div className="container">
+
+        <h1>WELCOME TO YOUR NEXT DOG-E-DATE</h1>
      
-        <h1>Image</h1>
-        
+      <button className="SignUp">SIGN UP</button>
+
       </div>
       
     </div>
